95948c7b855c8b9226b58747b8be20021ff58a12
Lars Ellenberg <lars.ellenberg@linbit.com>
2024-10-10 19:01:26 +0200

    Fix for 'Revert "drbd: rework autopromote"'
    That revert was incomplete/incorrect, and introduced possible deadlocks:
    
      # echo >/dev/drbdX </dev/drbdX &
    would deadlock in drbd_release() -> drbd_fsync_device() -> drbd_reject_write_early(),
    and prevent any future access to the device until reboot.
    
      # drbdsetup primary X
      # mount -o ro /dev/drbdX /mnt
      # mount -o remount,rw /mnt
      # umount /mnt
      # drbdsetup down X
    would leave the device "un-down-able" and locked in primary role.
    If you repeat that long enough, you may even provoke a kernel crash.
    
      # drbdsetup resource-options --auto-promote=no X
      # drbdsetup secondary X
      # : > /dev/drbdX
    would leave the device "un-down-able".
    Again, with a little creativity you likely could provoke a kernel crash.
    
    Introduce device->ro_cnt_is_write to handle the "remount" case.
    
    Call drbd_fsync_device() before we decrease the open_*_cnt
    to fix the potential deadlock.
    
    Fix the compat layer to use the passed in "mode" instead of hardcoded 0
    for drbd_release() in the error path of drbd_open().

--- drbd/drbd_int.h
+++ drbd/drbd_int.h
@@ -1543,6 +1543,7 @@ struct drbd_device {
 
 	struct drbd_bitmap *bitmap;
 
+	bool ro_cnt_is_write; /* we want to deal with mount --remount,rw somehow */
 	int open_rw_cnt, open_ro_cnt;
 
 	/* FIXME clean comments, restructure so it is more obvious which
